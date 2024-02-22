1c8e7f8dba1ed8fc9c57ad4029b0c4f4cf646b01
Luan <luan.ag2012@hotmail.com>
2024-02-21 22:09:28 -0300

    att

---
+++ src/CalculadoraFinanceira.php
@@ -0,0 +1,84 @@
+<?php 
+
+
+
+class CalculadoraFinanceira {
+    
+    public float $capital;
+    public float $taxa;
+    public int $tempo;
+    public string $tipo;
+
+    public function verificacao($capital, $taxa, $tempo) {
+
+        if (!is_numeric($capital) || !is_numeric($taxa) || !is_numeric($tempo)) {
+            throw new InvalidArgumentException("Todas as entradas devem ser válidas.");
+        }
+
+        if ($capital < 0 || $taxa < 0 || $tempo < 0) {
+            throw new InvalidArgumentException("Todos os valores devem ser positivos.");
+        }
+
+        if ($taxa > 30 || $tempo > 100) {
+            throw new InvalidArgumentException("Todos os valores devem estar em um intervalo razoável.");
+        }
+    }
+
+    public function calcularJurosSimples($capital, $taxa, $tempo) {
+        
+        $this->verificacao($capital, $taxa, $tempo);
+
+        $taxaDecimal = $taxa/100;
+
+        $juros = $capital * $taxaDecimal * $tempo;
+
+        return round($juros, 2);
+    }
+
+    public function calcularJurosCompostos($capital, $taxa, $tempo){
+        $this->verificacao($capital, $taxa, $tempo);
+
+        $taxaDecimal = $taxa/100;
+
+        $juros = ($capital * (1 + $taxaDecimal)**$tempo) - $capital;
+
+        return round($juros, 2);
+    }
+
+    public function calcularAmortizacao($capital, $taxa, $tempo, $tipo) {
+        $this->verificacao($capital, $taxa, $tempo);
+
+        $jurosTotal = 0;
+        $parcelasAmortizacao = array();
+
+        $taxaDecimal = $taxa/100;
+
+        switch($tipo) {
+
+            case "SAC":
+                $amortizacao = $capital / $tempo;
+                for ($i = 1; $i <= $tempo; $i++) {
+                    $juros = $capital * $taxaDecimal;
+                    $jurosTotal += $juros;
+                    $parcelasAmortizacao[] = round($amortizacao + $juros, 2);
+                    $capital -= $amortizacao;
+                }
+                break;
+
+            case "Price":
+                $amortizacao = $capital * $taxaDecimal * pow(1 + $taxaDecimal, $tempo) / (pow(1 + $taxaDecimal, $tempo) - 1);
+                for ($i = 1; $i <= $tempo; $i++) {
+                    $juros = $capital * $taxaDecimal;
+                    $jurosTotal += $juros;
+                    $parcelasAmortizacao[] = round($amortizacao, 2);
+                    $capital -= $amortizacao - $juros;
+                }
+                break;
+
+            default:
+                throw new InvalidArgumentException("Escolha um tipo de amortização válido.");
+        }
+
+        return array(round($jurosTotal, 2), $parcelasAmortizacao);
+    }
+}
---
+++ tests/CalculadoraFinanceiraTest.php
@@ -0,0 +1,131 @@
+<?php
+
+require_once "./src/CalculadoraFinanceira.php";
+use PHPUnit\Framework\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+
+class CalculadoraFinanceiraTest extends TestCase {
+
+    private readonly CalculadoraFinanceira $CalculadoraFinanceira;
+
+    public function setup(): void {
+        $this->CalculadoraFinanceira = new CalculadoraFinanceira;
+    }
+
+    //calcularJurosSimples
+
+    public function testCalcularJurosSimplesValoresPositivos() {
+        $resposta = $this->CalculadoraFinanceira->calcularJurosSimples(1289, 5, 3);
+
+        $this->assertEquals(193.35, $resposta);
+    }
+
+    public function testCalcularJurosSimplesValoresNegativos() {
+        $this->expectException(InvalidArgumentException::class);
+
+        $resposta = $this->CalculadoraFinanceira->calcularJurosSimples(-1231, -1, -1);
+
+        $this->assertEquals(1234, $resposta);
+    }
+
+    public function testCalcularJurosSimplesValoresExtremos() {
+        $this->expectException(InvalidArgumentException::class);
+
+        $resposta = $this->CalculadoraFinanceira->calcularJurosSimples(2000, 32, 120);
+
+        $this->assertEquals(1111, $resposta);
+    }
+
+    public function testCalcularJurosSimplesEntradaInválida() {
+        $this->expectException(InvalidArgumentException::class);
+
+        $resposta = $this->CalculadoraFinanceira->calcularJurosSimples("mil", "10/100", "2013");
+
+        $this->assertEquals(1111, $resposta);
+    }
+
+    //calcularJurosCompostos
+
+    public function testCalcularJurosCompostosValoresPositivos() {
+        $resposta = $this->CalculadoraFinanceira->calcularJurosCompostos(2000, 7, 5);
+
+        $this->assertEquals(805.10, $resposta);
+    }
+
+    public function testCalcularJurosCompostosValoresNegativos() {
+        $this->expectException(InvalidArgumentException::class);
+
+        $resposta = $this->CalculadoraFinanceira->calcularJurosCompostos(-2000, -7, -5);
+
+        $this->assertEquals(805.10, $resposta);
+    }
+
+    public function testCalcularJurosCompostosValoresExtremos() {
+        $this->expectException(InvalidArgumentException::class);
+
+        $resposta = $this->CalculadoraFinanceira->calcularJurosCompostos(12000, 321, 1200);
+
+        $this->assertEquals(1111, $resposta);
+    }
+
+    public function testCalcularJurosCompostosEntradaInválida() {
+        $this->expectException(InvalidArgumentException::class);
+
+        $resposta = $this->CalculadoraFinanceira->calcularJurosCompostos("doze", "percentual", "");
+
+        $this->assertEquals(1111, $resposta);
+    }
+
+    //calcularAmortizacao
+
+    public function testCalcularAmortizacaoSAC() {
+        $resposta = $this->CalculadoraFinanceira->calcularAmortizacao(10000, 5, 2, "SAC");
+
+        $this->assertEquals([750, [5500, 5250]], $resposta);
+    }
+
+    public function testCalcularAmortizacaoPrice() {
+        $resposta = $this->CalculadoraFinanceira->calcularAmortizacao(10000, 5, 2, "Price");
+
+        $this->assertEquals([756.10, [5378.05, 5378.05]], $resposta);
+    }
+
+    public function testCalcularAmortizacaoValoresNegativos() {
+        $this->expectException(InvalidArgumentException::class);
+
+        $resposta = $this->CalculadoraFinanceira->calcularAmortizacao(-10000, -5, -2, "Price");
+
+        $this->assertEquals([123, [4234, 4234]], $resposta);
+    }
+
+    public function testCalcularAmortizacaoValoresExtremos() {
+        $this->expectException(InvalidArgumentException::class);
+
+        $resposta = $this->CalculadoraFinanceira->calcularAmortizacao(10000, 50, 200, "SAC");
+
+        $this->assertEquals([0532, [90123, 12312]], $resposta);
+    }
+
+    public function testCalcularAmortizacaoEntradaInválida() {
+        $this->expectException(InvalidArgumentException::class);
+
+        $resposta = $this->CalculadoraFinanceira->calcularAmortizacao("um", "dois", "", "SAC");
+
+        $this->assertEquals([9034, [3901, 4821]], $resposta);
+    }
+
+    public function testCalcularAmortizacaoTipoInválido() {
+        $this->expectException(InvalidArgumentException::class);
+
+        $resposta = $this->CalculadoraFinanceira->calcularAmortizacao(20000, 6, 3, "sac");
+
+        $this->assertEquals([9348, [1922, 4572]], $resposta);
+    }
+
+    
+
+
+
+
+}
